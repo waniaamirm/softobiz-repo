87cc2aa900476df6b012677d720ebf9ac48171cd
waniaamirm <Aamir.Mushtaq@softobiz.com>
2019-08-19 16:33:08 +0530

    helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
-public static void main(String[] args) {
-//Prints "Hello, Word" to the terminal window.
-Systems.out.printlin("Hello, World");
-}
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
